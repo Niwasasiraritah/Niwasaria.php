2fb442fa1ef1a367ee4e7447dba76fdb0df920e8
Niwasasiraritah <niwasasiraritah934@gmail.com>
2024-09-20 17:25:17 +0300

    Create Niwasaria.php

---
+++ Niwasaria.php
@@ -0,0 +1,16 @@
+<?php 
+$Name = "NIWASASIRA RITAH";
+$DOB = "08/12/2002";
+$Address = "NYABUSHABI-KABALE";
+$current_year = 2024;
+$year_of_birth = 2002;
+$age = $current_year - $year_of_birth;
+$welcome = "welcome back home";
+
+echo "Name: " . $Name . "\n";
+echo "Date of birth: " . $DOB . "\n";
+echo "Home Address: " . $Address . "\n";
+echo "Welcome! madam, you are now " . $age . " years old.\n";
+echo "you are" . $welcome . "\n";
+
+?>
